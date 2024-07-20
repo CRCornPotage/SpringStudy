d65c4065959e9f7ed48c72543d0e21b393273464
CRCornPotage <subaru20011107@gmail.com>
2024-07-20 12:47:52 +0900

    formの入れ子完了

--- SpringStudy/src/main/java/com/example/web/controller/TestNestedObjectController.java
+++ SpringStudy/src/main/java/com/example/web/controller/TestNestedObjectController.java
@@ -3,6 +3,7 @@ package com.example.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domein.service.TestNestedObjectService;
@@ -25,6 +26,17 @@ public class TestNestedObjectController {
 		model.addAttribute(
 				testNestedObjectService.initCreateSelectForm(
 						new CreateEnqueteForm()));
+		
+		return "nested/index";
+		
+	}
+	
+	@PostMapping
+	public String objectToString(CreateEnqueteForm createEnqueteForm, Model model) {
+		
+		createEnqueteForm.toString();
+		
+		model.addAttribute(createEnqueteForm);
 		
 		return "nested/index";
 		
